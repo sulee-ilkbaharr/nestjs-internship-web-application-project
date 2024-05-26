f94b32581b530be9dddbaa69b33d77f746c86e59
Nilhantopcu <nilhantopcu9@gmail.com>
2024-05-26 18:53:39 +0300

    report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -10,8 +10,6 @@ import {
 import { ReportsService } from './reports.service';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { v4 as uuidv4 } from 'uuid';
-import { extname } from 'path';
 import { Response } from 'express';
 
 @Controller('reports')
@@ -24,8 +22,7 @@ export class ReportsController {
       storage: diskStorage({
         destination: './uploads',
         filename: (req, file, callback) => {
-          const uniqueSuffix = `${uuidv4()}${extname(file.originalname)}`;
-          callback(null, uniqueSuffix);
+          callback(null, file.originalname);
         },
       }),
     }),
